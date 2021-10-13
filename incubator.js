94d26a3844b924e08748926eb656ab5a044c5d72
Aldo Lopez <lopez.aldo.a@gmail.com>
2021-10-13 17:57:30 -0400

    5 min

--- incubator.js
+++ incubator.js
@@ -8,7 +8,7 @@ const device = new TuyAPI({
   issueGetOnConnect: false,
 });
 
-const INTERVAL = 10000;
+const INTERVAL = 300000;
 const MAX_TEMP = 120;
 const MIN_TEMP = 85;
 const SENSOR_TYPE = 22;
