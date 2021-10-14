ce3c1fb34ef1ab2e07dba509f744e4d757493950
Aldo Lopez <lopez.aldo.a@gmail.com>
2021-10-13 20:32:09 -0400

    logging

--- incubator.js
+++ incubator.js
@@ -1,6 +1,17 @@
 require('dotenv').config();
 const TuyAPI = require('tuyapi');
 const sensor = require('node-dht-sensor').promises;
+const winston = require('winston');
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
 
 const device = new TuyAPI({
   id: process.env.DEVICE_ID,
@@ -23,34 +34,35 @@ async function exec() {
   try {
     await connect();
     let on = await device.get();
-    console.log(`Current status: ${on}`);
+    logger.info(`Current status: ${on}`);
 
     try {
       const res = await sensor.read(SENSOR_TYPE, PIN_LOCATION);
       const fTemp = res.temperature * (9 / 5) + 32;
-      console.log(
+      logger.info(
         `Temp: ${fTemp.toFixed(1)}°F\nHumidity: ${res.humidity.toFixed(1)}%`
       );
       if (fTemp > MAX_TEMP && on) {
-        console.log('cooling down');
+        logger.info('cooling down');
         await device.set({ set: false });
       } else if (fTemp < MIN_TEMP && !on) {
-        console.log('heating up');
+        logger.info('heating up');
         await device.set({ set: true });
       }
     } catch (err) {
-      console.log(err);
+      logger.error(err);
     }
   } catch (err) {
-    console.log(err);
+    logger.error(err);
   } finally {
+    logger.info('Disconnecting');
     device.disconnect();
   }
 }
 
 const startDate = new Date();
 const endDate = new Date(new Date().getTime() + 60 * 60 * 1000 * 24);
-console.log(`Ending on ${endDate.toISOString()}`);
+logger.info(`Ending on ${endDate.toLocaleDateString()}`);
 setInterval(async () => {
   if (startDate < endDate) {
     exec();
